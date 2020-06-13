7ca417567c36a08666c320e12a728098d4a1ce7f
rickytorrez <torrez.ricky@ymail.com>
2020-06-13 12:49:39 -0400

    log/reg routes

---
+++ client/src/context/auth/AuthState.js
@@ -0,0 +1,54 @@
+import React, { useReducer } from 'react';
+import AuthContext from './authContext';
+import authReducer from './authReducer';
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAIL,
+  USER_LOADED,
+  AUTH_ERROR,
+  LOGIN_SUCCESS,
+  LOGIN_FAIL,
+  LOGOUT,
+  CLEAR_ERRORS,
+} from '../types';
+
+const AuthState = (props) => {
+  const initalState = {
+    token: localStorage.getItem('token'),
+    isAuthenticated: null,
+    loading: true,
+    user: null,
+    error: null,
+  };
+
+  const [state, dispatch] = useReducer(authReducer, initalState);
+
+  // actions \\
+
+  // load user
+
+  // register user
+
+  // login user
+
+  // logout user
+
+  // clear errors
+
+  // return provider to wrap the app with context
+  return (
+    <AuthContext.Provider
+      value={{
+        token: state.token,
+        isAuthenticated: state.isAuthenticated,
+        loading: state.loading,
+        user: state.user,
+        error: state.error,
+      }}
+    >
+      {props.children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthState;
