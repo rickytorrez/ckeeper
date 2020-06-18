89200883c0b355d2076ce9096bf999dd7f329c38
rickytorrez <torrez.ricky@ymail.com>
2020-06-18 19:55:04 -0400

    authContext finished, logout func added

--- client/src/context/auth/AuthState.js
+++ client/src/context/auth/AuthState.js
@@ -96,6 +96,9 @@ const AuthState = (props) => {
   };
 
   // logout user
+  const logout = () => {
+    dispatch({ type: LOGOUT });
+  };
 
   // clear errors
   const clearErrors = () => {
@@ -115,6 +118,7 @@ const AuthState = (props) => {
         clearErrors,
         loadUser,
         login,
+        logout,
       }}
     >
       {props.children}
--- client/src/context/auth/authReducer.js
+++ client/src/context/auth/authReducer.js
@@ -31,6 +31,7 @@ export default (state, action) => {
     case REGISTER_FAIL:
     case AUTH_ERROR:
     case LOGIN_FAIL:
+    case LOGOUT:
       // remove the token from storage on any failed reg
       localStorage.removeItem('token');
       return {
